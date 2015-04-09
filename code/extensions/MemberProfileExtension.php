60461ba8f85966e1afcd2b30745fe9b8dbd20c55
Jamie Barker <nimeso@gmail.com>
2015-04-09 13:14:36 +1200

    Rename ProfileLink > getProfileLink
    MemberRegistrationForm > register tries to call getProfileLink

--- code/extensions/MemberProfileExtension.php
+++ code/extensions/MemberProfileExtension.php
@@ -12,7 +12,7 @@ class MemberProfileExtension extends DataExtension{
 		);
 	}
 	
-	public function ProfileLink($action = null) {
+	public function getProfileLink($action = null) {
 		if($directorypage = MembersDirectoryPage::get()->first()){
 			return Controller::join_links(
 				$directorypage->Link(),
